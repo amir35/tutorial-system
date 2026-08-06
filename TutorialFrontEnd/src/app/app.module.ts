3f5b9dd8be70ad45588d5695437b42f6e9a59376
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2026-08-06 09:56:21 +0300

    Initial enterprise project

---
+++ TutorialFrontEnd/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TutorialDetailsComponent,
+    TutorialsListComponent,
+    AddTutorialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ tutorial-service/src/main/java/com/tutorials/tutorial_service/TutorialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.tutorial_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialServiceApplication.class, args);
+	}
+
+}
