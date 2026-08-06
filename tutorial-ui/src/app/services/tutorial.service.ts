2ea86d34388dab6cbfce91c5a59ded82286abfd1
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2026-08-06 10:14:33 +0300

    Adding backend url

--- tutorial-ui/src/app/services/tutorial.service.ts
+++ tutorial-ui/src/app/services/tutorial.service.ts
@@ -3,8 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-//const baseUrl = 'http://localhost:8080/api/tutorials';
-const baseUrl = 'http://tutorial.local/api/tutorials';
+const baseUrl = 'http://localhost:8080/api/tutorials';
+// const baseUrl = 'http://tutorial.local/api/tutorials';
 
 @Injectable({
   providedIn: 'root',
