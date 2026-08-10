18bc899c222b8794190a33735023b0350efe1e5a
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2026-08-10 09:26:07 +0300

    Updating UI Project name

--- tutorial-ui/src/app/app.component.spec.ts
+++ tutorial-ui/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TutorialFrontEnd'`, () => {
+  it(`should have as title 'TutorialUi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TutorialFrontEnd');
+    expect(app.title).toEqual('TutorialUi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TutorialFrontEnd app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('TutorialUi app is running!');
   });
 });
